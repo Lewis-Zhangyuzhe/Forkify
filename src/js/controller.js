7d18b720fecd8d4175267ac53f2268e5033934e7
zyz <zyz951123@gmail.com>
2021-08-03 15:20:58 -0700

    Refactoring to MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model.js';
+import recipeView from './views/RecipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -7,5 +13,26 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+////////////////////////////////////////////////////////////////
 
-console.log('test');
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1 Loading recipe
+    await model.loadRecipe(id);
+
+    // 2 Rending recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(`${err} from controller`);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
